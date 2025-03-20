fe9ed2ac2e2f5c153e539c7fdd99feabb6f414b1
JVM <jentejulian2009@gmail.com>
2025-03-20 21:16:22 +0000

    test model 1

---
+++ BEP.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+static_DB = {"Jente":"1234","Etnej":"4321"}
+
+@app.route("/")
+def index():
+    page_status = "Not Connected"
+    return render_template("FEP.html", page_status = page_status)
+
+@app.route("/conStat", methods=["POST"])
+def conStat():
+    page_status = "Connected"
+    return page_status
+
+@app.route("/infoHandler", methods=["POST"])
+def infoHandler():
+    if request.method == "POST":
+        in_SDB = False
+        info_list = request.form
+        name = info_list.get("name")
+        password = info_list.get("password")
+        DBM = info_list.get("debug")
+        SDB_pass = static_DB.get(name)
+        if SDB_pass == password:
+            in_SDB = True
+        if DBM == "true":
+            form_status = f"The backend has received the data. (debug: --n.[{name}] --p.[{password}] --ISDB[{in_SDB}] --DBM[{DBM}])"
+        elif DBM == "false":
+            form_status = "The backend has received the data."
+        else:
+            form_status = "Not all settings have been set correctly."
+        return_data = {"form_status":form_status,"ISDB":in_SDB}
+        return return_data
+
+if __name__ == '__main__':
+    app.run( 
+        host="codespaces-d57c39",
+        port=5001, 
+        debug=True
+        )
